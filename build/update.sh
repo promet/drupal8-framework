f8ffd8e665598120b87b037197dbe0770ee494ee
Allan Chappell <generalredneck@gmail.com>
2015-12-15 12:04:53 -0600

    updating Drupal 8 build scripts with Changes made to Drupal 7 build Scripts.

--- build/update.sh
+++ build/update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
-path=$(dirname "$0")
-source $path/common.sh
+path="$(dirname "$0")"
+source "$path/common.sh"
+
+# Change to the Drupal Directory Just In Case
+pushd "$drupal_base"
+
 # This was added because of upgrades like Rules 2.8 to 2.9 and Feeds alpha-9 to beta-1 where
 # new code and database tables are added and running other code will cause white screen until
 # the updates are run.
@@ -18,5 +22,5 @@ $drush cim sync
 echo "Clearing caches one last time.";
 $drush cr
 
-chmod -R +w $base/cnf
-chmod -R +w $base/www/sites/default
+chmod -R +w "$base/cnf"
+chmod -R +w "$base/www/sites/default"
